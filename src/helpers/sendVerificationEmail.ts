2dc39d7a12554c6fcd9972e9989d9f4daf7228a7
Suryansh Bhatnagar <sbh2068@gmail.com>
2024-05-06 22:03:57 +0530

    fix  sendVerificationEmail function

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,6 @@
-import { resend } from "@/lib/resend";
-import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from '@/types/ApiResponse';
+import nodemailer from 'nodemailer'
+
 
 export async function sendVerificationEmail(
   email: string,
@@ -8,12 +8,25 @@ export async function sendVerificationEmail(
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
-    await resend.emails.send({
-      from: 'onboarding@resend.com',
-      to: email,
-      subject: 'Secret Message Verification Code',
-      react: VerificationEmail({ username, otp: verifyCode }),
-    });
+
+      let transporter = nodemailer.createTransport({
+          host: process.env.MAIL_HOST,
+          port: 587,
+          secure: false,
+          auth: {
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS
+          }
+      })
+
+      let info = await transporter.sendMail({
+          from:  process.env.MAIL_USER,
+          to: `${email}`,
+          subject: `Verify your code`,
+          html: `<p>Your verification code is  ${verifyCode}</p>`,
+      })
+      console.log('Sent mail info ', info);
+
     return { success: true, message: 'Verification email sent successfully.' };
   } catch (emailError) {
     console.error('Error sending verification email:', emailError);
